f852af3eb79564d27d73ca626c24797ddaa92231
Mammen Mathew <mammen999@gmail.com>
2024-11-07 23:43:11 +0530

    find by email bug fixed

--- Octaview-server/src/infrastructure/repositories/UserRepository.ts
+++ Octaview-server/src/infrastructure/repositories/UserRepository.ts
@@ -6,11 +6,10 @@ export class UserRepository implements IuserRepository {
     async create(user: UserEntity): Promise<void> {
         await UserModal.create(user)
     }
-  // src/infrastructure/repositories/UserRepository.ts
+  
 async findByUserEmail(email: string): Promise<UserEntity | null> {
-    console.log("Finding user with email:", email);  // Log the email
+    console.log("email:", email);  
     const user = await UserModal.findOne({ email });
     return user ? new UserEntity(user.email, user.password, user.companyName) : null;
 }
-
 }
