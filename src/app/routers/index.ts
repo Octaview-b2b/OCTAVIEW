6ced148090638341a50e459469e60866d33549e4
Mammen Mathew <mammen999@gmail.com>
2024-11-29 11:52:22 +0530

    add job api

--- src/app/routers/index.ts
+++ src/app/routers/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userAuthRouter from './user/auth_router';
+import JobRouter from './user/jobs_router';
 
 const router = Router();
 
 router.use('/user', userAuthRouter);
+router.use('/jobs',JobRouter)
 
 export default router;
--- src/app/routers/user/jobs_router.ts
+++ src/app/routers/user/jobs_router.ts
@@ -3,13 +3,13 @@ import { JobControllers } from "../../controllers/user/job_Controllers";
 import { JobUseCase } from "../../../core/use-cases/user/JobUseCase";
 import { JobRepository } from "../../../infrastructure/repositories/JobRepository";
 import asyncHandler from "express-async-handler";
-const router = express.Router()
+const JobRouter = express.Router()
 
 const jobRepository = new JobRepository()
 const jobUsecase = new JobUseCase(jobRepository)
 const job_Controllers = new JobControllers(jobUsecase)
 
-router.post(
-    "/users/:userId/jobs",
-    asyncHandler((req, res) => job_Controllers.createJob(req, res))
-  );
+JobRouter.post("/:userId",asyncHandler((req, res) => job_Controllers.createJob(req, res)));
+
+export default JobRouter
+
