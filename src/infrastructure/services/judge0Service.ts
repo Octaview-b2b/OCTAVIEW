f98f305038e9978d0be87e7325720d65c530bc7c
Mammen Mathew <mammen999@gmail.com>
2025-03-11 13:34:28 +0530

    Deploying latest changes

--- src/infrastructure/services/judge0Service.ts
+++ src/infrastructure/services/judge0Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { JUDGE0_BASE_URL } from "../../config/env";
 
-// let url = 'http://localhost:2358'
+// let url = 'http://localhost:2358' is only for testing
 
 
 export class Judge0Service {
